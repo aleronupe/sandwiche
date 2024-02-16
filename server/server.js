db5eaeb0585ebb689e0ea84613ec5e8a696775b6
Alexandre Miguel <alexandre.miguel@portaldecompraspublicas.com.br>
2024-02-15 23:08:31 -0300

    Add style to Table

--- server/server.js
+++ server/server.js
@@ -7,7 +7,28 @@ app.use(express.json());
 app.get('/api/data', (req, res) => {
   // Implemente a lógica para obter dados reais aqui
   const data = { clicks: [50, 30, 60, 45, 75, 90, 40] };
-  res.json(data);
+
+  const dados = [
+    {
+      date: "2023-12-01",
+      pageViews: 30,
+      clickedLinks: {
+        "link-01": 20,
+        "link-02": 5,
+        "link-03": 5
+      }
+    }, {
+      date: "2023-12-02",
+      pageViews: 20,
+      clickedLinks: {
+        "link-01": 10,
+        "link-02": 3,
+        "link-03": 7
+      }
+    }
+  ]
+
+  res.json(dados);
 });
 
 app.listen(port, () => {
