9ab657dd768c8dab00ce7c9831adaeb2429984b8
Alexandre Miguel <alexandre.miguel@portaldecompraspublicas.com.br>
2024-02-15 22:40:50 -0300

    Remove chart reference

--- DailyCounter/src/app/app.module.ts
+++ DailyCounter/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChartComponent } from './chart/chart.component';
 import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChartComponent,
     TableComponent
   ],
   imports: [
